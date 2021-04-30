75248212a52f326951a42ac89976557a42fe1376
Camille Cutrone <cjcutroneiii@gmail.com>
2021-04-30 10:29:28 -0500

    feat: create User Model

---
+++ Expenses.Data.Model/Authentication/Users/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Expenses.Data.Model.Authentication.Users
+{
+    public class User
+    {
+        public Guid? Id { get; set; }
+        public string Username { get; set; }
+
+        public User(string username)
+        {
+            this.Username = username;
+        }
+    }
+}

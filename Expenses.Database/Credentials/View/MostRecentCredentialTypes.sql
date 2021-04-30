94152f287db697d6651067e9cca625064a470a2e
Camille Cutrone <cjcutroneiii@gmail.com>
2021-04-30 10:28:42 -0500

    feat: create MostRecentCredentialTypes view

---
+++ Expenses.Database/Credentials/View/MostRecentCredentialTypes.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [Expenses].[MostRecentCredentialTypes]
+AS
+SELECT 
+	latest.[UserId],
+	latest.[CredentialTypeId]
+FROM (
+	SELECT
+		cred.[UserId],
+		cred.[CredentialTypeId],
+		MAX(cred.[CreatedOn]) [CreatedOn]
+	FROM [Expenses].[Credential] cred
+	WHERE cred.[ExpiredOn] IS NULL
+		OR cred.[ExpiredOn] >= SYSDATETIMEOFFSET()
+	GROUP BY cred.[UserId], cred.[CredentialTypeId]
+) latest;

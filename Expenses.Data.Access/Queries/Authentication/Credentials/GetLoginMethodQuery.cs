caa2f3b6ee385f054c6723d983553e91e4e7fc3f
Camille Cutrone <cjcutroneiii@gmail.com>
2021-04-30 11:14:35 -0500

    feat: add GetLoginMethodQuery

---
+++ Expenses.Data.Access/Queries/Authentication/Credentials/GetLoginMethodQuery.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using Expenses.Data.Model.Authentication.Credentials;
+using Expenses.Data.Model.Authentication.Users;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.Data.Access.Queries.Authentication.Credentials
+{
+    public class GetLoginMethodQuery
+        : IExpenseQuery<User, List<CredentialType>>
+    {
+        public async Task<List<CredentialType>> OnExecute(DbConnection connection, User user)
+        {
+            var response = (await connection
+                .QueryAsync<CredentialType>(GetLoginMethod, new { username = user.Username }))
+                .ToList();
+
+            return response;
+        }
+
+        #region GetLoginMethod
+        public const string GetLoginMethod =
+@"
+SELECT 
+	ct.[Id],
+	ct.[Name],
+	ct.[Code],
+	ct.[Description]
+FROM [Expenses].[MostRecentCredentialTypes] latest
+INNER JOIN [Expenses].[Credential.Type] ct
+    ON ct.[Id] = latest.[CredentialTypeId]
+INNER JOIN [Expenses].[User] users
+	ON users.[Id] = latest.[UserId]
+WHERE users.[Username] = @username;
+";
+        #endregion
+    }
+}

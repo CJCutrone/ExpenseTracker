6cc25c100e22411847505386b891d332835c472b
Camille Cutrone <cjcutroneiii@gmail.com>
2021-05-18 09:46:09 -0500

    feat: create UserRole and add to User

--- Expenses.Data.Model/Authentication/Users/User.cs
+++ Expenses.Data.Model/Authentication/Users/User.cs
@@ -5,7 +5,14 @@ namespace Expenses.Data.Model.Authentication.Users
     public class User
     {
         public Guid? Id { get; set; }
+
         public string Username { get; set; }
+        public string Email { get; set; }
+
+        public UserRole Role { get; set; }
+
+        public User() 
+        { }
 
         public User(string username)
         {
---
+++ Expenses.Data.Model/Authentication/Users/UserRole.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Expenses.Data.Model.Authentication.Users
+{
+    public class UserRole
+    {
+        public Guid? Id { get; set; }
+        public string Code { get; set; }
+    }
+}

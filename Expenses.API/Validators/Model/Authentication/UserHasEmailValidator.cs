825abc38a07f09a08e369d57a349b98bab5ca80b
Camille Cutrone <cjcutroneiii@gmail.com>
2021-05-20 09:19:44 -0500

    feat: add GeneratePasswordResetTokenRequestValidator

---
+++ Expenses.API/Validators/Model/Authentication/UserHasEmailValidator.cs
@@ -0,0 +1,19 @@
+﻿using Expenses.Data.Model.Authentication.Users;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.API.Validators.Model.Authentication
+{
+    public class UserHasEmailValidator
+        : AbstractValidator<User>
+    {
+        public UserHasEmailValidator()
+        {
+            this.RuleFor(it => it.Email)
+                .NotEmpty();
+        }
+    }
+}
---
+++ Expenses.API/Validators/Request/Authentication/Credentials/GeneratePasswordResetTokenRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Expenses.API.Controllers.Authentication.Credentials.Request;
+using Expenses.API.Validators.Model.Authentication;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expenses.API.Validators.Request.Authentication.Credentials
+{
+    public class GeneratePasswordResetTokenRequestValidator
+        : AbstractValidator<GeneratePasswordResetTokenRequest>
+    {
+        public GeneratePasswordResetTokenRequestValidator()
+        {
+            this.RuleFor(it => it.User)
+                .NotNull()
+                .SetValidator(new UserHasEmailValidator());
+        }
+    }
+}

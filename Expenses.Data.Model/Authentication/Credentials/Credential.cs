abfbd08b9d0d15dd11289422c0ea9d8d1775f09a
Camille Cutrone <cjcutroneiii@gmail.com>
2021-05-20 13:15:28 -0500

    feat: add Credential Model and update CredentialType

---
+++ Expenses.Data.Model/Authentication/Credentials/Credential.cs
@@ -0,0 +1,19 @@
+﻿using Expenses.Data.Model.Authentication.Users;
+using System;
+
+namespace Expenses.Data.Model.Authentication.Credentials
+{
+    public class Credential
+    {
+        public User User { get; set; }
+        public User CreatedBy { get; set; }
+
+        public CredentialType Type { get; set; }
+
+        public string Salt { get; set; }
+        public string Hash { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset ExpiresOn { get; set; }
+    }
+}
--- Expenses.Data.Model/Authentication/Credentials/CredentialType.cs
+++ Expenses.Data.Model/Authentication/Credentials/CredentialType.cs
@@ -4,6 +4,14 @@ namespace Expenses.Data.Model.Authentication.Credentials
 {
     public class CredentialType
     {
+        public CredentialType()
+        { }
+
+        public CredentialType(string code)
+        {
+            this.Code = code;
+        }
+
         public Guid Id { get; set; }
 	    public string Name { get; set; }
 	    public string Code { get; set; }
